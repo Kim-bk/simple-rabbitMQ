31e0df3285955fd9c8e1adbeb6d9d102a097a3fb
Kim-bk <nguyenhoangkim120201@gmail.com>
2022-09-21 16:41:51 +0700

    add Producer vs Consumer

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Consumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+
+            using var chanel = connection.CreateModel();
+            chanel.QueueDeclare("demo-queue",
+                durable: true,
+                autoDelete: false,
+                exclusive: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(chanel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            chanel.BasicConsume("demo-queue", true, consumer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Producer/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Producer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var chanel = connection.CreateModel();
+            chanel.QueueDeclare("demo-queue",
+                durable: true,
+                autoDelete: false,
+                exclusive: false,
+                arguments: null);
+
+            var message = new { Named = "Producer", Message = "Hello" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            chanel.BasicPublish("", "demo-queue", null, body);
+        }
+    }
+}
